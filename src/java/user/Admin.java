111790a8b6157c0f89c0e7e2ed41a4d7510cb2b9
eli88p <eli88popik@gmail.com>
2015-05-18 18:59:00 +0300

    Eli

---
+++ src/java/user/Admin.java
@@ -0,0 +1,12 @@
+package user;
+
+
+import user.SystemUser;
+
+
+public class Admin extends SystemUser {
+
+    public Admin(String fn, String ln, String un, String pass, String email) {
+        super(fn, ln, un, pass, email);
+    }
+}
